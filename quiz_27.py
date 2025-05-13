5a7155e30a545bf571758176e4fcec2ea67cc961
notime <wpong.debug@gmail.com>
2025-05-13 17:49:57 +0000

    quiz 27

---
+++ quiz_27.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums[:] = filter(lambda x: x != val, nums)]
+        return len(nums)
+    

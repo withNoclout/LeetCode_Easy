757a257efda48dd57e47eb3e5b6504be07c98db0
notime <wpong.debug@gmail.com>
2025-06-23 08:59:37 +0000

    Added 3 new quiz files: NumberReport, LargestNumber, MaximumTime.

---
+++ quiz_1736_maximumTime.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maximumTime(self, time):
+        """
+        :type time: str
+        :rtype: str
+        """
+        time = list(time)
+        if time[0] == '?' : 
+            if time[1] == '?' or time[1] < '4':
+                time[0] = '2'
+            else:
+                time[0] = '1'
+
+        if time[1]  == '?':
+            if time[1] == '3' if time[0] =='2' else '9' 
+
+        if time[3]  == '?' : 
+            time[3 ] = '5' 
+
+        if time[4] =='?' :
+            time[4] = '9'
+
+
+        return ''.join(time)
